00c480e8c720d4cf79566d4f67bba60d8331d883
josevilca10 <145402561+josevilca10@users.noreply.github.com>
2025-07-06 08:49:34 -0500

    Creacion de las carpetas src

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mi-proyecto-angular';
+  name = "JOSE ARMANDO VILCA MAMANI";
+  email = "jvilcamam@unsa.edu.pe";
+  webpage = "http://www.unsa.edu.pe";
+  hobbies: string[] = [];
+  nuevoHobby: string = ''; 
+
+  constructor() { }
+
+  showHobbies(): boolean {
+    return this.hobbies.length > 0;
+  }
+
+  addHobby() {
+    if (this.nuevoHobby.trim() !== '') {
+      this.hobbies.push(this.nuevoHobby.trim());
+      this.nuevoHobby = '';
+    }
+  }
+}
